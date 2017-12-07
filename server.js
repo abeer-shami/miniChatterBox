53dd66342215942fa53c67ae6f6d0abbc951dbcb
abeer-shami <abeer.shami887@gmail.com>
2017-12-07 22:13:56 +0000

    saving the messages to mongo db

--- server.js
+++ server.js
@@ -3,27 +3,49 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+var dbUrl= 'mongodb://user:0000@ds133816.mlab.com:33816/node'
+
+var Message = mongoose.model('Message',{
+	name:String,
+	message:String
+})
+
 var messages = [
     {name: 'Abeer', message: 'still working'},
     {name: 'Kenan', message: 'still crying'}
 ]
+
 app.get('/messages', (req, res) =>{
-    res.send(messages)
+	Message.find({}, (err,messages) =>{
+		res.send(messages)
+	})
+    
 })
 
 app.post('/messages', (req, res) =>{
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+	var message = new Message(req.body)
+	message.save((err) =>{
+		if(err)
+			sendStatus(500)
+
+    	io.emit('message', req.body)
+    	res.sendStatus(200)
+	})
+
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbUrl,{useMongoClient:true},(err) =>{
+	console.log('mongoose connected', err)
 })
 
 http.listen(2017, ()=>{
