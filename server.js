9d244b439462cf431b1a82bc7405a8bc786fa1e3
abeer-shami <abeer.shami887@gmail.com>
2017-12-07 20:10:03 +0000

    creat a get request

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var app = express()
 
 app.use(express.static(__dirname))
 
+var messages = [
+    {name: 'Abeer', message: 'still working'},
+    {name: 'Kenan', message: 'still crying'}
+]
+app.get('/messages', (req, res) =>{
+    res.send(messages)
+})
+
 app.listen(3000, ()=>{
 	console.log('server listening on port 3000')
 })
